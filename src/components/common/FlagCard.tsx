fe7d5d0e9c2a5af0730beafae58d2ca43229d9e9
pksingh1508 <rajiit1212@gmail.com>
2025-09-26 22:03:11 +0530

    fix: flag card issue

--- src/components/common/FlagCard.tsx
+++ src/components/common/FlagCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { fontPoppins } from "@/fonts";
+import Link from "next/link";
 
 interface FlagCardProps {
   flagImageUrl: string;
@@ -9,24 +10,28 @@ interface FlagCardProps {
 
 const FlagCard: React.FC<FlagCardProps> = ({ flagImageUrl, countryName }) => {
   return (
-    <div className="bg-gray-50 rounded-lg shadow-md overflow-hidden border border-gray-200 hover:shadow-lg transition-shadow duration-200 max-w-32 p-2 flex flex-col items-center justify-center">
-      <div className="bg-white rounded-lg p-1 px-auto shadow-lg">
-        <Image
-          src={flagImageUrl}
-          alt={`${countryName} flag`}
-          width={24}
-          height={16}
-          className="w-24 h-16 object-cover rounded"
-        />
+    <Link href={"/contact"}>
+      <div className="bg-gray-50 rounded-lg shadow-md overflow-hidden border border-gray-200 hover:shadow-lg transition-shadow duration-200 max-w-32 max-h-40 w-full h-full p-2">
+        <div className="flex flex-col items-center justify-center">
+          <div className="bg-white rounded-lg p-1 px-auto shadow-lg">
+            <Image
+              src={flagImageUrl}
+              alt={`${countryName} flag`}
+              width={24}
+              height={16}
+              className="w-24 h-16 object-cover rounded"
+            />
+          </div>
+          <div className="p-4">
+            <h3
+              className={`text-lg font-semibold text-gray-800 text-center ${fontPoppins.className}`}
+            >
+              {countryName}
+            </h3>
+          </div>
+        </div>
       </div>
-      <div className="p-4">
-        <h3
-          className={`text-lg font-semibold text-gray-800 text-center ${fontPoppins.className}`}
-        >
-          {countryName}
-        </h3>
-      </div>
-    </div>
+    </Link>
   );
 };
 
